0c481db1216a8f2407a16ac15bec6f1c0c45e97b
VincenzoBorriello89 <vincborr89@gmail.com>
2022-04-12 17:57:25 +0200

    header fatto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderComp/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComp from './components/HeaderComp.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComp
   }
 }
 </script>
 
 <style lang="scss">
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  // text-align: center;
+  // color: #2c3e50;
+  // margin-top: 60px;
 }
+
 </style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,112 @@
+<template>
+    <header>
+        <a href="">
+            <img src="../assets/img/dc-logo.png" alt="DC Logo">
+        </a>
+        <ul>
+            <li v-for="link, i in links" :key="i" :class="link.here ? 'active' : '' ">
+                <a :href="link.url">{{link.text}}</a>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComp',
+    data(){
+        return{
+            links: [
+                {
+                    text: 'CHARACTERS',
+                    url: '#',
+                    here: false,
+                },
+                {
+                    text: 'COMICS',
+                    url: '#',
+                    here: true,
+                },
+                {
+                    text: 'MOVIES',
+                    url: '#',
+                    here: false,
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    here: false,
+                },
+                {
+                    text: 'GAMES',
+                    url: '#',
+                    here: false,
+                },
+                {
+                    text: 'COLLECTIBLES',
+                    url: '#',
+                    here: false,
+                },
+                {
+                    text: 'VIDEOS',
+                    url: '#',
+                    here: false,
+                },
+                {
+                    text: 'FANS',
+                    url: '#',
+                    here: false,
+                },
+                {
+                    text: 'NEWS',
+                    url: '#',
+                    here: false,
+                },
+                {
+                    text: 'SHOP',
+                    url: '#',
+                    here: false,
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 80%;
+    margin: 0 auto;
+    img {
+        padding: 20px 0;
+    }
+    li {
+        display: inline-block;
+        margin: 0 15px;
+        transition: all .15s ease;
+        padding: 62px 0;
+        a {
+            padding: 62px 0;
+            text-decoration: none;
+            color: black;
+            font-weight: bold;
+        }
+        &.active {
+            a {
+                color: #0282f9;
+            }
+            border-bottom: 5px solid #0282f9;
+        }
+        &:hover {
+            a {
+                color: #0282f9;
+            }
+            border-bottom: 5px solid #0282f9;
+        }
+    }
+}
+</style>
