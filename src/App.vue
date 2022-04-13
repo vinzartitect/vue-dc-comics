437f8f18529b48fbd70eb2a87409a9a54fa20017
VincenzoBorriello89 <vincborr89@gmail.com>
2022-04-13 12:15:52 +0200

    main da riempire fatto

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <HeaderComp/>
+    <MainComp/>
   </div>
 </template>
 
 <script>
 import HeaderComp from './components/HeaderComp.vue';
+import MainComp from './components/MainComp.vue';
 
 export default {
   name: 'App',
   components: {
-    HeaderComp
+    HeaderComp,
+    MainComp
   }
 }
 </script>
---
+++ src/components/MainComp.vue
@@ -0,0 +1,28 @@
+<template>
+    <main>
+        <div>
+            <h1>-->Content goes here!!!</h1>
+        </div>        
+    </main>
+</template>
+
+<script>
+
+export default {
+    name: 'MainComp',
+    components: {
+        
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+main {
+    background-color: #000000;
+    color: white;
+    h1 {
+        padding: 50px 15%;
+    }
+}
+</style>
