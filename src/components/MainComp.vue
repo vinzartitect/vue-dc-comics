c0df63107bbb35a998944ab3b87995d65eaa7dfb
VincenzoBorriello89 <vincborr89@gmail.com>
2022-04-19 16:02:37 +0200

    sistemaz bottoni e comicsbook

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,19 +1,19 @@
 <template>
     <main>
         <JumboTron/>
-        <!-- <ComicsBook/>            -->
+        <ComicsBook/>
     </main>
 </template>
 
 <script>
 import JumboTron from '../components/JumboTron.vue'
-// import ComicsBook from '../components/ComicsBook.vue'
+import ComicsBook from '../components/ComicsBook.vue'
 
 export default {
     name: 'MainComp',
     components: {
         JumboTron,
-        // ComicsBook,       
+        ComicsBook,       
     }
 }
 </script>
@@ -21,7 +21,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 main {
-    background-color: #000000;
+    background-color: #1c1c1c;
     color: white;
     h1 {
         padding: 50px 15%;
